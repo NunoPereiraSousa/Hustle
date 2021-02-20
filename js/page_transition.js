df8dc8d2d6a9021791f7914f2ba32f53432bcdb4
Nuno Sousa <9180579@esmad.ipp.pt>
2021-02-20 17:19:32 +0000

    Page transition working

--- js/page_transition.js
+++ js/page_transition.js
@@ -1,18 +1,93 @@
 barba.init({
+  sync: true,
   transitions: [
     {
-      name: "default-transition",
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0
-        });
+      async leave(data) {
+        const done = this.async();
+
+        pageTransition();
+
+        await delay(1000);
+        done();
       },
-      enter(data) {
-        // create your amazing enter animation here
-        return gsap.to(data.next.container, {
-          opacity: 1
-        });
+
+      async enter(data) {
+        // onLoad();
+        contentAnimation();
+      },
+
+      async once(data) {
+        onLoad();
       }
     }
   ]
 });
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise(done => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+function pageTransition() {
+  let tl = gsap.timeline();
+
+  tl.to(".line", {
+    y: "100%",
+    stagger: 0.25,
+    duration: 1,
+    ease: "Expo.easeInOut"
+  })
+    .to(
+      ".loading",
+      {
+        duration: 1.2,
+        height: "100%",
+        ease: "Expo.easeInOut"
+      },
+      "-=1"
+    )
+    .to(".loading", {
+      bottom: "100%"
+    })
+    .set(".loading", {
+      bottom: "0%",
+      height: "0%"
+    });
+}
+
+function contentAnimation() {
+  let tl = gsap.timeline();
+
+  tl.to(".line", {
+    duration: 0.65,
+    y: 0,
+    stagger: 0.25,
+    delay: 1.75,
+    ease: "expo.easeInOut"
+  });
+}
+
+function onLoad() {
+  let tl = gsap.timeline();
+
+  tl.to(".line", {
+    duration: 0.65,
+    y: 0,
+    stagger: 0.25,
+    delay: 0.5,
+    ease: "expo.easeInOut"
+  });
+}
+
+// window.onload = () => {
+//   gsap.to(".line", {
+//     duration: 0.65,
+//     y: 0,
+//     stagger: 0.25,
+//     ease: "expo.easeinout"
+//   });
+// };
