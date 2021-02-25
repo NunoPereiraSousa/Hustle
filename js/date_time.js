00c8f292f8590f7ac1a892e38d9226ee2fe2487e
Nuno Sousa <9180579@esmad.ipp.pt>
2021-02-25 19:20:19 +0000

    Working on contact section

---
+++ js/date_time.js
@@ -0,0 +1,7 @@
+// let date = new Date();
+// let hours = date.getHours();
+// let minutes = date.getMinutes();
+
+// let current_time = document.querySelector("#current_time");
+
+// current_time.innerHTML = `${hours}:${minutes}.`;
